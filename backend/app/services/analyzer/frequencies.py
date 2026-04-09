4938d7238e477684e9b37cb1097ae5b5382f98b0
Mateusz <mswierczek121@gmail.com>
2026-04-08 19:49:39 -0700

    Added MAQ handling

--- backend/app/services/analyzer/frequencies.py
+++ backend/app/services/analyzer/frequencies.py
@@ -10,12 +10,12 @@ def generate_frequencies_table(mapping:list[Question]) -> Generator[pd.DataFrame
         if col.cafeteria is None:
             yield _create_value_counts_table(df[col.question], col)
         elif col.subquestions is not None:
-            print(col.is_maq)
             if col.is_maq:
                 complex_table = _create_maq_table(col.subquestions, df, col)
             else:
                 complex_table = _create_matrix_table(col.subquestions, df, col)
-            yield complex_table #type:ignore
+            assert complex_table is not None
+            yield complex_table
         else:
             yield _create_value_counts_table(
                 pd.Categorical(df[col.question], 
@@ -29,17 +29,23 @@ def _create_value_counts_table(question:pd.Series | pd.Categorical | pd.DataFram
 #Refaktoryzacja + % w kolumnach
 def _create_maq_table(subquestions:list[Question], df:pd.DataFrame, col:Question):
     detector = Detector()
-    temp_cafe = {cafe["index"]:cafe["value"] for cafe in col.cafeteria_dump} #type: ignore
-    main_cafeteria_mapping_sorted = dict(sorted(temp_cafe.items()))
+    assert col.cafeteria_dump
+    main_mapping = {cafe["value"]:cafe["index"] for cafe in col.cafeteria_dump}
     subquestions_columns = [subq.question for subq in subquestions]
     matrix_df = df[subquestions_columns]
     matrix_df.columns = [detector.get_cafeteria_item(matrix_col) for matrix_col in matrix_df.columns]
-    print(matrix_df)
+    for column in matrix_df.columns:
+        matrix_df[column] = matrix_df[column].map(main_mapping)
+    matrix_df = matrix_df.sum().reset_index(name="Częstości")
+    matrix_df["% z N"] = matrix_df["Częstości"] / col.total_count
+    matrix_df["% z Odpowiedzi"] = (matrix_df["Częstości"] / matrix_df["Częstości"].sum()).round(2)
+    return matrix_df
 
 #TODO: Refaktoryzacja + % w kolumnach
 def _create_matrix_table(subquestions:list[Question], df:pd.DataFrame, col:Question):
     detector = Detector()
-    temp_cafe = {cafe["index"]:cafe["value"] for cafe in col.cafeteria_dump} #type: ignore
+    assert col.cafeteria_dump
+    temp_cafe = {cafe["index"]:cafe["value"] for cafe in col.cafeteria_dump}
     main_cafeteria_mapping_sorted = dict(sorted(temp_cafe.items()))
     subquestions_columns = [subq.question for subq in subquestions]
     matrix_df = df[subquestions_columns]
